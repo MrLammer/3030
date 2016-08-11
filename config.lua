a7e02cc03d56388d14f57abd4813e8303facfa2e
TELE BN <neorabbit.m@gmail.com>
2016-08-11 11:09:41 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,7 +12,7 @@ return {
 	},
 	log_chat = -1001057801239,
 	channel = '@LammerTeam', --channel username with the '@'
-	help_group = 'https://telegram.me/joinchat/CHYUej9jFxFN12MQqrLnUg', --group link, not username!
+	help_group = 'https://telegram.me/joinchat/BuGEqj-pypcbeAxJEQVX6w', --group link, not username!
 	languages = 'languages.lua',
 	plugins = {
 		'onmessage.lua', --THIS HAVE TO BE THE FIRST: IF AN USER IS SPAMMING/IS BLOCKED, THE BOT WON'T GO THROUGH PLUGINS
